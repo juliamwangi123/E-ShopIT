b5c063ab7f6173c90e02101b2bfc8fd5bfa3ff4d
Julie <juliamwangi123>
2023-05-13 17:45:05 +0300

    add loader

--- src/components/pages/auth/home/Products.js
+++ src/components/pages/auth/home/Products.js
@@ -22,8 +22,8 @@ function Products() {
     const products = useSelector(getRandomProducts)
     
   return (
-    <div>
-       {loading && <div>loading ...</div>}
+    <div className='flex items-center justify-center'>
+       {loading && <div className='Loader'></div>}
         {error && <div>{error}</div>}
         <div className=" flex flex-wrap -mx-4">
           {products &&products.map(product => (
@@ -31,14 +31,16 @@ function Products() {
           <div className="bg-white rounded-lg shadow p-4 h-full flex flex-col">
             <img src={product.image} alt={product.name} className="w-full h-48 object-contain mb-4 rounded-lg" />
             {/* <h2 className="font-bold text-lg mb-2">{product.name}</h2> */}
-            <Link className='flex-grow  mb-[5px] description' to={`/${product._id}`}>
+            <Link className='flex-grow  mb-[5px] description' to={`product/${product._id}`}>
             <p className="text-gray-500 flex-grow   hover:text-[#f87622]">{product.description.length > 100 ? `${product.description.substr(0, 70)}...` : product.description}</p>
             </Link>
             <div className='flex justify-between '>
             <p className="text-[#c00] font-[700] text-[16px] ">${product.price.toFixed(2)}</p>
             <p className='bg-[#f87622] text-[white] px-2'> Save 15% </p>
             </div>
-            <button className="bg-[#f87622] text-white text-[18px] font-[700] px-4 py-[8px] mt-2  ">Buy now</button>
+            <Link className='flex-grow ' to={`product/${product._id}`}>
+            <button className="bg-[#f87622] w-full text-white text-[18px] font-[700] px-4 py-[8px] mt-2  ">Buy now</button>
+            </Link>
           </div>
         </div>
       ))}
