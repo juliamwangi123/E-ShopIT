44be26d62928a77574131ede56037d4312ec0930
Julie <juliamwangi123>
2023-05-15 19:51:44 +0300

    form validation

--- src/components/pages/auth/Signin.js
+++ src/components/pages/auth/Signin.js
@@ -60,7 +60,11 @@ useEffect (()=>{
             {formError && !password && <p className="text-red-500">{formError}</p>}
         </label>
          
-        <button type="submit" className="w-full flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-orange-500 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+        <button type="submit" 
+                className={`w-full flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-sm 
+                font-medium text-white  bg-orange-500  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
+                // disabled={!email || !password}
+                >
           {loading && <div>loading ....</div>}
           SIGN UP
         </button>
