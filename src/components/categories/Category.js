64d41a521567eeea786d3283d4908962636148a8
Julie <juliamwangi123>
2023-05-16 16:59:10 +0300

    filter by category

--- src/components/categories/Category.js
+++ src/components/categories/Category.js
@@ -1,19 +1,44 @@
+import { useSelector } from "react-redux"
 
 const Category = () =>{
+    const {products} = useSelector((state) => state.products)
+
+    const handleCategory = (cat) =>{
+      const c=  products.filter((product =>{
+            return product.category === cat
+        }));
+
+        console.log(c)
+    }
+
     return(
-        <div className="categories text-gray-500  ">
+        <div className="categories flex-wrap text-gray-500  ">
             <small className="hidden sm:inline mr-[6px]">Top Searches: </small>
-            <small className="mr-[5px]">Men</small> 
-            <small className="mr-[5px]">Women</small>
-            <small className="mr-[5px]">Jewelry</small>
-            <small className="mr-[5px]">Electronics</small> 
+            <small 
+                className="mr-[5px]"
+                onClick={()=>{handleCategory("men's clothing")}}>
+                Men
+            </small> 
+            <small 
+                className="mr-[5px]"
+                onClick={()=>{handleCategory("women's clothing")}}>
+                Women
+            </small>
+            <small 
+                className="mr-[5px]"
+                onClick={()=> handleCategory('jewelery')}>
+                Jewelry
+            </small>
+            <small 
+                className="mr-[5px]"
+                onClick={()=>{handleCategory("electronics")}}>
+                Electronics
+            </small> 
             <small className="mr-[5px]">Phone</small> 
             <small className="mr-[5px]">Watches</small> 
             <small className="mr-[5px]">Handbags</small> 
             <small className="mr-[5px]">Wigs</small> 
             <small className="mr-[5px]">Shoes</small> 
-
-
       </div>
     )
 }
